8f377d714adb1bb110691aa26d337f25cabbcba1
James Craig <craigj2@carleton.edu>
2020-12-18 12:07:26 -0600

    add msgpack

---
+++ springdemo/src/main/java/com/example/springdemo/models/MessagePacket.java
@@ -0,0 +1,11 @@
+package com.example.springdemo.models;
+
+public class MessagePacket {
+		private String message;
+			public MessagePacket(String message) {
+						this.message = message;
+							}
+				public String getMessage(){
+							return message;
+								}
+}
