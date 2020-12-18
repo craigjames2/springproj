fcff09b3c8ee4313c2fad2ebb08c6c02e4d26f57
craigjames2 <55596172+craigjames2@users.noreply.github.com>
2020-12-18 10:52:26 -0600

    Update AppController.java

--- springdemo/src/main/java/com/example/springdemo/controllers/AppController.java
+++ springdemo/src/main/java/com/example/springdemo/controllers/AppController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.*;
 import com.example.springdemo.services.HelloService;
 
 @RestController
-@RequestMapping("/app")
+@RequestMapping("/pulsar")
 public class AppController {
 	@Autowired
 	HelloService helloService;
-
+	PulsarService pulsarService;
+	
 	@GetMapping("/hello")
 	public String getHello() {
 		return helloService.getHello();
+	}
+	
+	@PostMapping(("/postmessage")
+	public String postMessage(@RequestBody String newMessage) {
+		return pulsarService.postMessage(newMessage);
 	}
 }
