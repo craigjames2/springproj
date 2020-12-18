1b8bf86d1be719a948394bcc983f21c467cd52ba
craigjames2 <55596172+craigjames2@users.noreply.github.com>
2020-12-18 13:50:24 -0600

    Update MessagePacket.java

--- springdemo/src/main/java/com/example/springdemo/models/MessagePacket.java
+++ springdemo/src/main/java/com/example/springdemo/models/MessagePacket.java
@@ -1,11 +1,14 @@
 package com.example.springdemo.models;
 
 public class MessagePacket {
-		private String message;
-			public MessagePacket(String message) {
-						this.message = message;
-							}
-				public String getMessage(){
-							return message;
-								}
+	private String message;
+	private MessagePacket() {
+	}
+	public MessagePacket(String message) {
+		this();
+		this.message = message;
+	}
+	public String getMessage(){
+		return message;
+	}
 }
